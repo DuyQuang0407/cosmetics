c3220eb9ba5177d360dd12da4ead45e7180905cb
Quang Cao <quangcdse140367@fpt.edu.vn>
2021-11-01 19:26:27 +0700

    create function go top

--- app/javascript/controllers/main_controller.js
+++ app/javascript/controllers/main_controller.js
@@ -4,7 +4,9 @@ export default class extends Controller {
     static targets = ['main']
     static lozadLoaded = false
 
-    connect() {}
+    connect() {
+        this.init()
+    }
 
     mainTargetConnected() {
         if (!this.lozadLoaded) {
@@ -23,4 +25,27 @@ export default class extends Controller {
     disconnect() {}
 
     initialize() {}
+
+    init() {
+        let self = this;
+        self.navbarWillFixed(window.scrollY);
+        window.addEventListener("scroll", function() {
+            self.navbarWillFixed(window.scrollY);
+        });
+    }
+
+    navbarWillFixed(scrollY) {
+        if (scrollY >= 800) {
+            document.getElementById("goTop").classList.add("showGoTop");
+        }
+
+        if (scrollY == 0) {
+            document.getElementById("goTop").classList.remove("showGoTop");
+        }
+    }
+
+    goTop(event) {
+        event.preventDefault();
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    }
 }
